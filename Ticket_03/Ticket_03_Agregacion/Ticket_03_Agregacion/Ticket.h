f33c7257daa2101163e622f371488df2c6427789
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-05-30 18:26:51 -0300

    ADD - Ticket_03

---
+++ Ticket_03/Ticket_03_Agregacion/Ticket_03_Agregacion/Ticket.h
@@ -0,0 +1,78 @@
+
+#ifndef __TICKET_H__
+
+	#define __TICKET_H__
+
+	#include <string>
+	
+	#include "../../../Lib/Lib_Fecha/Lib_Fecha_Agregacion/Fecha.h"
+	#include "../../../Lib/Lib_Hora/Lib_Hora_Agregacion/Hora.h"
+
+	using namespace std;
+
+	class Ticket
+	{
+		public:
+			// Constructor
+			Ticket();
+			Ticket(const string);
+			
+			// Destructor
+			~Ticket();
+
+			// Asignar Atributo
+			void AsignarDatos(const string);
+
+			// Obtener Atributo
+			string ObtenerDatos() const;
+			string ObtenerNro() const;
+			Fecha & ObtenerFecha();
+			Hora & ObtenerHora();
+
+			// Servicio
+			int ValidarDatos();
+			void MostrarDatos();
+			void MostrarDatos(const string);
+
+		private:
+			string datos;
+			string nro;
+			Fecha fecha;
+			Hora hora;
+			
+			// Servicio Interno
+			void ParsearDatos();
+	};
+
+	// Asignar Atributo
+	inline void Ticket::AsignarDatos(const string datos)
+	{
+		this->datos = datos;
+		
+		this->ParsearDatos();
+	
+		return;
+	}
+
+	// Obtener Atributo
+	inline string Ticket::ObtenerDatos() const
+	{
+		return(this->datos);
+	}
+	
+	inline string Ticket::ObtenerNro() const
+	{
+		return(this->nro);
+	}
+	
+	inline Fecha & Ticket::ObtenerFecha()
+	{
+		return(this->fecha);
+	}
+	
+	inline Hora & Ticket::ObtenerHora()
+	{
+		return(this->hora);
+	}
+
+#endif
---
+++ Ticket_03/Ticket_03_Generalizacion/Ticket_03_Generalizacion/Ticket.h
@@ -0,0 +1,69 @@
+
+#ifndef __TICKET_H__
+
+	#define __TICKET_H__
+
+	#include <string>
+	
+	#include "../../../Lib/Lib_Hora/Lib_Hora_Generalizacion/Hora.h"
+
+	using namespace std;
+
+	class Ticket : public Hora
+	{
+		public:
+			// Constructor
+			Ticket();
+			Ticket(const string);
+			
+			// Destructor
+			~Ticket();
+
+			// Asignar Atributo
+			void AsignarDatos(const string);
+
+			// Obtener Atributo
+			string ObtenerDatos() const;
+			string ObtenerNro() const;
+			Hora * ObtenerHora();
+
+			// Servicio
+			int ValidarDatos();
+			void MostrarDatos();
+			void MostrarDatos(const string);
+
+		private:
+			string datos;
+			string nro;
+			
+			// Servicio Interno
+			void ParsearDatos();
+	};
+
+	// Asignar Atributo
+	inline void Ticket::AsignarDatos(const string datos)
+	{
+		this->datos = datos;
+		
+		this->ParsearDatos();
+	
+		return;
+	}
+
+	// Obtener Atributo
+	inline string Ticket::ObtenerDatos() const
+	{
+		return(this->datos);
+	}
+	
+	inline string Ticket::ObtenerNro() const
+	{
+		return(this->nro);
+	}
+
+	inline Hora * Ticket::ObtenerHora()
+	{
+		return (Hora *) this;
+	}
+
+#endif
